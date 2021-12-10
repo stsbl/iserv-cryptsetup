7b599abcdfbcfc2a7bce690870c6bff09f9eabc0
Felix Jacobi <Felix@jacobi-hamburg.net>
2021-12-10 16:27:05 +0100

    Fixed lvm file descriptor warning, added missing dependency, updated lvm argument handling in cryptroot

--- iconf/etc/initramfs-tools/conf.d/cryptroot/00cryptsetup.pl
+++ iconf/etc/initramfs-tools/conf.d/cryptroot/00cryptsetup.pl
@@ -22,11 +22,21 @@ $args{source} = "UUID=$root{cryptroot_parent}->{uuid}";
 $args{key} = "/etc/keys/cryptkey.gpg";
 $args{keyscript} = "/lib/cryptsetup/scripts/decrypt_gnupg_sc";
 
+# Suppress "File descriptor 10 [...] leaked on lvs invocation" warning
+$ENV{LVM_SUPPRESS_FD_WARNINGS} = 1;
+
 if ($root{root_device}->{type} eq "lvm")
 {
   my $q_device = shell_quote $root{root_device}->{path};
-  $args{lvm} = (qx(lvs --noheadings -o vg_name $q_device) =~ s/^ +//gr);
-  chomp $args{lvm};
+  my $vg_name = (qx(lvs --noheadings -o vg_name $q_device) =~ s/^ +//gr);
+  chomp $vg_name;
+  push @{ $args{lvm} }, $vg_name;
+  push @{ $args{lvm} }, $root{root_device}->{name};
 }
 
-print join(",", map { $_ . "=" . $args{$_} } keys %args) . "\n";
+print join(",", map {
+    my $arg = $_;
+    ref $args{$_} eq "ARRAY" ?
+        map { $arg . "=" . $_ } @{ $args{$_} } :
+        $_ . "=" . $args{$_}
+    } keys %args) . "\n";
