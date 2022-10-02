1e0bebd8221937dfbca9a3448d7e115ec04da1ed
Felix Jacobi <Felix@jacobi-hamburg.net>
2022-10-02 21:26:43 +0200

    Added support for static configuration, Added basic support for VLANs

--- iconf/etc/initramfs-tools/conf.d/net/20cryptsetup.sh
+++ iconf/etc/initramfs-tools/conf.d/net/20cryptsetup.sh
@@ -4,9 +4,27 @@
 
 if [ -n "$InitramfsNetworkInterface" ]
 then
-cat <<EOT
-DEVICE=$InitramfsNetworkInterface
-IP=:::::$InitramfsNetworkInterface:dhcp
+  IFACE="$InitramfsNetworkInterface"
+  # VLAN interface in $IFACE.$VALID_ID style, enable VLAN support
+  # TODO: vlan{num} style is not supported, figure out how to add in a sensible
+  # way. Also not supported in initramfs-tools-extras yet.
+  if [[ $IFACE =~ ^[^.]+\.[0-9]+$ ]]
+  then
+    IFS="." IFACE_PARTS=($IFACE)
+    printf "VLAN=${IFACE_PARTS[0]}:${IFACE_PARTS[1]}\n"
+  fi
+
+  IP=":::::$IFACE:dhcp"
+
+  if [ "$InitramfsNetworkConfigureStatic" ]
+  then
+    GW_IP="$(ip -json -4 route show default | jq --arg IFACE "$IFACE" -r '.[] | select(.dev==$IFACE) | .gateway')"
+    IP="$(netquery "if ip::$GW_IP:mask:" | awk "\$1 == \"$IFACE\" { print \$2 }" | head -n 1):$(head -n 1 /etc/hostname)"
+  fi
+
+  cat <<EOT
+DEVICE=$IFACE
+IP=$IP
 
 EOT
 else
